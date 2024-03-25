237275c83de37af283f7b357038650a954808515
Code <idulsa.emmannicholasblabe.000@gmail.com>
2024-03-25 10:11:55 +0800

    Progress:

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,6 +1,17 @@
 import Link from "next/link";
+// import PocketBase from 'pocketbase';
+import styles from './Notes.module.css';
+
+// export const dynamic = 'auto',
+//     dynamicParams = true,
+//     revalidate = 0,
+//     fetchCache = 'auto',
+//     runtime = 'nodejs',
+//     preferredRegion = 'auto'
 
 async function getNotes() {
+    // const db = new PocketBase('http://127.0.0.1:8090/');
+    // const data = await db.records.getList('Note');
     const res = await fetch('http://127.0.0.1:8090/api/collections/Note/records?page=1&perPage=30',
         { cache: 'no-store' }
     );
@@ -26,8 +37,8 @@ function Note({ note }: any) {
     const { id, Title, Content, created } = note || {};
 
     return (
-        <Link href={'/Note/${id}'}>
-            <div>
+        <Link href={`/notes/${id}`}>
+            <div className={styles.note}>
                 <h2>{Title}</h2>
                 <h5>{Content}</h5>
                 <p>{created}</p>
